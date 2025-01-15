0bfe15e76c28cc1c682b493bc59d85d674e7211d
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-01-15 18:11:28 +0100

    moyenne fonction

--- index.js
+++ index.js
@@ -24,9 +24,23 @@ const moyenne = (notes) => {
   }
   return sum / notes.length;
 };
-moyenne([10, 20]);
+const compareStudent = (a, b) => {
+  //si a est superieur on renvoie 1 sinon -1
+  return b.moyenne - a.moyenne;
+};
 
 for (let student of students) {
   student.moyenne = moyenne(student.notes);
+  student.worst = Math.min(...student.notes);
+  student.best = Math.max(...student.notes);
 }
-console.log(students);
+students.sort(compareStudent);
+
+const formatStudent = (student) => {
+  return `${student.name} avec une moyenne de ${student.moyenne}`;
+};
+console.log(` Top 3 étudiant 
+    1: ${formatStudent(students[0])}
+    2: ${formatStudent(students[1])}
+    3: ${formatStudent(students[2])}
+    `);
