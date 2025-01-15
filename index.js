5c8ea808ed962e52fd2bcbb6551b49eb1afacf36
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-01-15 17:41:36 +0100

    function of moyenne

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-function isPalindrome(str) {
-  const reverseWord = str.split("").reverse().join("");
+const students = [
+  {
+    name: "John",
+    notes: [1, 20, 18, 19, 12],
+  },
+  {
+    name: "Jane",
+    notes: [17, 18, 20, 13, 15],
+  },
+  {
+    name: "Sophie",
+    notes: [17, 12, 14, 15, 13],
+  },
+  {
+    name: "Marc",
+    notes: [2, 3, 5, 8, 9],
+  },
+];
 
-  return str.toUpperCase() === reverseWord.toUpperCase();
-}
-
-const words = {
-  kayak: true,
-  SOS: true,
-  Kayak: true,
-  Bonjour: false,
-};
-for (let word in words) {
-  if (isPalindrome(word) !== words[word]) {
-    console.log(`isPalindrome(${word})`);
+const moyenne = (notes) => {
+  let sum = 0;
+  for (let note of notes) {
+    sum = sum + note;
   }
+  return sum / notes.length;
+};
+moyenne([10, 20]);
+
+for (let student of students) {
+  student.moyenne = moyenne(student.notes);
 }
+console.log(students);
